f3801cbc3aaad727f5619e2effc1086928b6b769
tanis <tk009@lvc.edu>
2024-11-21 14:26:48 -0500

    Abstract products created

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+// Abstract Products
+class Button {
+public:
+    // Render the button
+    virtual void render() = 0;
+    // Destructor
+    virtual ~Button() = default;
+};
+
+class Checkbox {
+public:
+    // Render the checkbox
+    virtual void render() = 0;
+    // Destructor
+    virtual ~Checkbox() = default;
+};
+
 int main() {
 
 }
