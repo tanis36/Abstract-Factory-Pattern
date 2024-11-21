3865f5aa8e0cc69cbbccce81648f01455e23d304
tanis <tk009@lvc.edu>
2024-11-21 14:31:13 -0500

    Concrete products created

--- main.cpp
+++ main.cpp
@@ -17,6 +17,39 @@ public:
     virtual ~Checkbox() = default;
 };
 
+// Concrete Products
+class AppleButton : public Button {
+public:
+    // Render Apple button
+    void render() override {
+        std::cout << "Rendering Apple Button" << std::endl;
+    }
+};
+
+class AndroidButton : public Button {
+public:
+    // Render Android button
+    void render() override {
+        std::cout << "Rendering Android Button" << std::endl;
+    }
+};
+
+class AppleCheckbox : public Checkbox {
+public:
+    // Render Apple checkbox
+    void render() override {
+        std::cout << "Rendering Apple Checkbox" << std::endl;
+    }
+};
+
+class AndroidCheckbox : public Checkbox {
+public:
+    // Render Android checkbox
+    void render() override {
+        std::cout << "Rendering Android Checkbox" << std::endl;
+    }
+};
+
 int main() {
 
 }
